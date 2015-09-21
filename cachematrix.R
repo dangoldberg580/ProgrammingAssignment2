c2e8d5aa8904325ed862970fb93fc4429b0e1503
dangoldberg580 <dan.goldberg@datadesignconsulting.com>
2015-09-21 19:09:50 -0400

    Scoping Exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix) {
+        ## set variables to outside the local environment
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## create a list of function calls
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+        
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' and cache the value
+        
+        m <- x$getInverse()
+        
+        ## check to see if Inverse has been set
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## calculate and return values
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
+
